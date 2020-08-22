5f48f12c7b56a91f7d199cd416b2c89d5787c88e
Nate Levesque <nate@thenaterhood.com>
2020-08-22 14:54:01 -0400

    demo server

--- setup.py
+++ setup.py
@@ -14,12 +14,18 @@ test_requires = [
     ]
 
 yapsy_files = glob.glob('src/stormberry/pluggable/*.*')
+server_files = glob.glob('src/stormberry/server/static/*.*')
+js_files = glob.glob('src/stormberry/server/static/assets/js/*.*')
+css_files = glob.glob('src/stormberry/server/static/assets/css/*.*')
 
 data_files =[
         ('/etc/stormberry', ['config.ini']),
         ('/lib/stormberry/plugins_available', ['README.md']),
         ('/lib/stormberry/plugins_enabled', ['README.md']),
-        ('/lib/stormberry/plugins_available', yapsy_files)
+        ('/lib/stormberry/plugins_available', yapsy_files),
+        ('/lib/stormberry/static_files', server_files),
+        ('/lib/stormberry/static_files/assets/js', js_files),
+        ('/lib/stormberry/static_files/assets/css', css_files)
         ]
 
 
@@ -33,13 +39,22 @@ setup(name='stormberry',
     tests_require=test_requires,
     entry_points={
         'console_scripts': [
-            'stormberry = stormberry.station.__main__:main'
+            'stormberry = stormberry.station.__main__:main',
+            'stormberry-demo-server = stormberry.server.server:demo'
         ]
     },
     test_suite='nose.collector',
     package_dir={'':'src'},
     packages=[
         'stormberry',
+        'stormberry.forecast',
+        'stormberry.interpreter',
+        'stormberry.server',
+        'stormberry.server.api',
+        'stormberry.server.api.grafana',
+        'stormberry.server.api.forecast',
+        'stormberry.server.api.comfort',
+        'stormberry.server.api.weather',
         'stormberry.station',
         'stormberry.plugin',
         'stormberry.pluggable',
--- src/stormberry/server/server.py
+++ src/stormberry/server/server.py
@@ -5,10 +5,11 @@ from stormberry.server.api.weather import weather_blueprint
 from stormberry.server.api.forecast import forecast_blueprint
 from stormberry.server.api.comfort import comfort_blueprint
 from stormberry.server.util import get_repository
+from stormberry.config import Config
 
 get_repository() # This raises an exception if something goes wrong so we won't start the server
 
-app = Flask(__name__, static_url_path='/assets', static_folder='static/assets')
+app = Flask(__name__, static_url_path='/assets', static_folder='/lib/stormberry/static_files/assets')
 app.register_blueprint(grafana_blueprint, url_prefix="/api/grafana")
 app.register_blueprint(weather_blueprint, url_prefix="/weather")
 app.register_blueprint(forecast_blueprint, url_prefix="/predict")
@@ -18,3 +19,21 @@ app.register_blueprint(comfort_blueprint, url_prefix="/comfort")
 def send_index():
     return send_from_directory('static', 'index.html')
 
+def demo():
+    '''
+    Run the Flask development server as a demonstration.
+    '''
+    print('=============================================')
+    print('If you are doing anything resembling production, DO NOT USE THIS.')
+    print('You should properly configure a server and run the application with WSGI')
+    print('=============================================')
+
+    config = Config()
+    port = None
+
+    try:
+        port = config.get("SERVER", "PORT")
+    except:
+        pass
+
+    app.run(port=port, host='0.0.0.0')
