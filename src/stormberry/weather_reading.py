16aff841c9916abf5b77bf7e1877f28e59caf2ea
Nate Levesque <nate@thenaterhood.com>
2020-08-29 23:13:25 -0400

    add pm10, fix None issue

--- src/stormberry/weather_reading.py
+++ src/stormberry/weather_reading.py
@@ -14,6 +14,7 @@ class WeatherReading():
             pressureInchesHg=None,
             wind_mph=None,
             pm_2_5=None,
+            pm_10=None,
             precipitation_cm=None,
             noise_dB=None
             ):
@@ -32,6 +33,7 @@ class WeatherReading():
         self.__date = date if date is not None else datetime.now()
         self.__wind_mph = wind_mph
         self.__pm_2_5 = pm_2_5
+        self.__pm_10 = pm_10
         self.__precipitation_cm = precipitation_cm
         self.__noise_dB = noise_dB
 
@@ -46,6 +48,10 @@ class WeatherReading():
     @property
     def pm_2_5(self):
         return self.__pm_2_5
+
+    @property
+    def pm_10(self):
+        return self.__pm_10
 
     @property
     def tempc(self):
@@ -88,6 +94,9 @@ class WeatherReading():
 
     @property
     def timestr(self):
+        if self.__date is None:
+            return None
+
         return self.__date.strftime("%Y-%m-%d %H:%M:%S")
 
     @property
@@ -96,10 +105,16 @@ class WeatherReading():
         Calculates dewpoint in celsius, uses simplified formula less accurate but obvious.
         https://en.wikipedia.org/wiki/Dew_point#Calculating_the_dew_point
         """
+        if self.__tempc is None or self.__humidity is None:
+            return None
+
         return self.__tempc - (100 - self.__humidity) / 5
 
     @property
     def dewpointf(self):
+        if self.__humidity is None or self.tempf is None:
+            return None
+
         return self.tempf - (100 - self.__humidity) / 5
 
     @property
@@ -115,6 +130,7 @@ class WeatherReading():
                 self.pressure_inHg,
                 self.wind_mph,
                 self.pm_2_5,
+                self.pm_10,
                 self.precipitation_cm,
                 self.noise_dB
                 )
@@ -132,6 +148,7 @@ class WeatherReading():
                 'dewpointf': self.dewpointf,
                 'wind_mph': self.wind_mph,
                 'pm_2_5': self.pm_2_5,
+                "pm_10": self.pm_10,
                 'precipitation_cm': self.precipitation_cm,
                 'noise_dB': self.noise_dB
                 }
@@ -148,7 +165,8 @@ class WeatherReading():
         self.__wind_mph = self.__wind_mph if self.__wind_mph is not None else weather_reading.wind_mph
         self.__pm_2_5 = self.__pm_2_5 if self.__pm_2_5 is not None else weather_reading.pm_2_5
         self.__precipitation_cm = self.__precipitation_cm if self.__precipitation_cm is not None else weather_reading.__precipitation_cm
-        self.__noise_dB = self.__noise_dB if self.__noise_dB is not None else weather_reading.noise_dB
+        self.__noise_dB = self.__noise_dB if self.__noise_dB is not None else weather_reading.noise_dB,
+        self.__pm_10 = self.__pm_10 if self.__pm_10 is not None else weather_reading.pm_10
 
     def __str__(self):
-        return (self.READINGS_PRINT_TEMPLATE % self.tuple)
+        return (self.READINGS_PRINT_TEMPLATE % self.tuple[0:5])
