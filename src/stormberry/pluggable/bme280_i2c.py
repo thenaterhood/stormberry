5efc9a17756a6e3c8ff06e14f676e0cd36b28c54
Nate Levesque <nate@thenaterhood.com>
2020-09-12 17:49:37 -0400

    cpu compensation for bme280

--- src/stormberry/pluggable/bme280_i2c.py
+++ src/stormberry/pluggable/bme280_i2c.py
@@ -10,6 +10,7 @@ from bme280 import BME280
 
 # See https://github.com/pimoroni/enviroplus-python
 # pip3 install enviroplus
+# Enable i2c. This assumes a "modern" pi which uses port 1.
 
 class BME280_I2C(stormberry.plugin.ISensorPlugin):
 
@@ -19,11 +20,24 @@ class BME280_I2C(stormberry.plugin.ISensorPlugin):
         self.config = config
         self.bus = SMBus(1)
         self.bme280 = BME280(i2c_dev=self.bus)
+        self.cpu_temps = [self.get_cpu_temperature()] * 5
+        try:
+            self.compensate_temp = self.config.getboolean("BME280", "CPU_COMPENSATE")
+        except:
+            self.compensate_temp = True
+
+        try:
+            self.compensate_temp_factor = self.config.getint("BME280", "CPU_COMPENSATION_FACTOR")
+        except:
+            self.compensate_temp_factor = 2.25
 
     def get_reading(self):
 
         try:
             tempc = self.bme280.get_temperature()
+            if self.compensate_temp:
+                tempc = self.compensate_temp(tempc)
+
             pressure = self.bme280.get_pressure()
             humidity = self.bme280.get_humidity()
 
@@ -36,4 +50,17 @@ class BME280_I2C(stormberry.plugin.ISensorPlugin):
         except Exception as e:
             print(e)
             return None
+
+    def get_cpu_temperature(self):
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp = f.read()
+            temp = int(temp) / 1000.0
+        return temp
+
+    def compensate_temperature(self, raw_temp):
+        # Based on code from https://github.com/pimoroni/enviroplus-python/blob/master/examples/compensated-temperature.py
+        self.cpu_temps = self.cpu_temps[1:] + [self.get_cpu_temperature()]
+        avg_cpu_temp = sum(self.cpu_temps) / float(len(self.cpu_temps))
+        comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / self.compensate_temp_factor)
+        return comp_temp
 
